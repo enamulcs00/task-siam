bf244264f0b31c8b9fe79840d651341af8d7bd90
Enamul haque <62995394+enamulcs00@users.noreply.github.com>
2020-09-14 09:21:38 +0530

    MyTaskSiamComp
    SiamComp

---
+++ src/app/innerpages/innerpages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-innerpages',
+  templateUrl: './innerpages.component.html',
+  styleUrls: ['./innerpages.component.css']
+})
+export class InnerpagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
